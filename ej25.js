54d6be6243150ec5049d182de4e55c07fd622b23
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-18 00:38:48 +0200

    Añadido el ejercicio25

---
+++ ej25.js
@@ -0,0 +1,9 @@
+/*Dado un string,ponerlo completo en mayusculas o minusculas dependiendo de si hay más mayusculas o minusculas por defecto en el string.
+Si hay más mayusculas ponerlo todo en mayus.
+Si hay más minusculas ponerlo todo en minus.
+
+
+Ejemplos:
+mayusMinus("JOAQuin")  Devuelve:JOAQUIN
+mayusMinus("Victor")  Devuelve:victor 
+*/
