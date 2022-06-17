c664c481f4b35a5b97fcfe2b5844a75473372930
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-17 22:12:41 +0200

    [x]Ejercicio 24 resuelto

--- ej24.js
+++ ej24.js
@@ -6,3 +6,18 @@ Ejemplos:
 permutacion([1,2,3,4,5], 5)  Devuelve true
 permutacion([1,2,3,5],5)     Devuelve false
 */
+
+// -Función que reciba dos parámetros, array y entero
+// -contar desde el 1 hasta el número
+// -volver esa cuenta un array
+// -comparar los arrays
+
+const permutacion = (array, num) => {
+  let count = [];
+  for (let i = 1; i <= num; i++) {
+    count.push(i);
+  }
+  return count.length == array.length;
+};
+
+console.log(permutacion([1, 2, 3, 5], 5));
