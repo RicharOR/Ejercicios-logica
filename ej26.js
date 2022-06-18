12db6be235ef19961ea825e8f26126f1ce177a03
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-18 12:09:31 +0200

    Añadido el ejercicio 26

---
+++ ej26.js
@@ -0,0 +1,8 @@
+/* Dado un número, mostrar su serie fibonacci.
+La serie fibonacci es un orden de números donde cada número es la suma de los dos aneteriores.
+
+Ejemplos:
+fib(10)[0]  Serie completa: 0,1,1,2,3,5,8,13,21,34,55
+fib(10)[1]  Resultado de fibonacci: 55
+
+*/
