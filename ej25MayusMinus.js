870a7b766c083f6c039ff67d23ff610a366bbc47
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-18 12:04:44 +0200

    [x]Ejercicio 25 con otra respuesta

--- ej25MayusMinus.js
+++ ej25MayusMinus.js
@@ -1,4 +1,4 @@
-/*Dado un string,ponerlo completo en mayusculas o minusculas dependiendo de si hay más mayusculas o
+/* Dado un string,ponerlo completo en mayusculas o minusculas dependiendo de si hay más mayusculas o
 minusculas por defecto en el string.
 Si hay más mayusculas ponerlo todo en mayus.
 Si hay más minusculas ponerlo todo en minus.
@@ -6,14 +6,40 @@ Si hay más minusculas ponerlo todo en minus.
 Ejemplos:
 mayusMinus("JOAQuin")  Devuelve:JOAQUIN
 mayusMinus("Victor")  Devuelve:victor 
+
+TODO: Cómo hacerlo:
+-Crear una función con un parámetro texto
+-Crear dos contadores para mayus y para minus
+-Recorrer el texto y comprobar si las letras que vamos recorriendo son mayus
+-Aumentar contadores
+-Convertir a mayus o minus y devolver el resultado
 */
 
+function mayusMinus(texto) {
+  let mayusculas = 0;
+  let minusculas = 0;
+  let resultado = '';
+
+  for (let letra of texto) {
+    if (/[A-Z]/.test(letra)) {
+      mayusculas++;
+    } else {
+      minusculas++;
+    }
+  }
+  if (mayusculas > minusculas) {
+    resultado = texto.toUpperCase();
+  } else {
+    resultado = texto.toLowerCase();
+  }
+  return resultado;
+}
 //-Crear una función que reciba un texto como parámetro
 //-Contar el tamaño del string
 //-Contar la cantidad de mayúsculas
 // -Si es mayor al la mitad de la longitud del estring colocarlo en mayúscula de lo contrario en minúscula
 
-const mayusMinus = txt => {
+const mayusOrMinus = txt => {
   const txtUpperCase = txt.toUpperCase();
   let matches = 0;
   for (let i = 0; i < txt.length; i++) {
@@ -27,5 +53,3 @@ const mayusMinus = txt => {
     return txt.toLowerCase();
   }
 };
-
-console.log(mayusMinus('JOAQuin'));
