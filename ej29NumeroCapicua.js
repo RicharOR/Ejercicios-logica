b4a6115aae082ef1aab260170fe11762d367f806
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-22 23:01:36 +0200

    [x]Creado el ejercicio 28

---
+++ ej29NumeroCapicua.js
@@ -0,0 +1,10 @@
+/* Dado un número indicar si es un número capicúa o no.
+
+Los número capicúa se leen igual de izquierda a derecha y viceversa
+
+Ejemplos:
+
+capicua(2002)  Devuelve true
+capicua(2014)  Devuelve false
+
+*/
