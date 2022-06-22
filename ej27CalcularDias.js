04c40b1b4256496ad1d6dcaae90c0a7cd14f3c01
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-22 22:44:19 +0200

    [x]Nombre del ej27 cambiado

---
+++ ej27CalcularDias.js
@@ -0,0 +1,12 @@
+/* Dado un número, mostrar a cuántos años, meses y días equivale 
+
+Ejemplo: 
+calcularFechas(920);  
+
+Devuelve:
+Equivale a 2 años, 6 meses y 10 días.
+
+TODO: Cómo hacerlo:
+-Crear una función con parámetro días
+-Divisiones para sacar años, meses y días
+*/
