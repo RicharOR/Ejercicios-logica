d41be1ab4b972c13a43ab976490a77adfcdfbb38
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-22 23:00:37 +0200

    [x]Creado el ejercico 28

---
+++ ej29NumeroCapicua.js
@@ -0,0 +1,17 @@
+/* Dado un número indicar si es un número capicúa o no.
+
+Los número capicúa se leen igual de izquierda a derecha y viceversa
+
+Ejemplos:
+
+capicua(2002)  Devuelve true
+capicua(2014)  Devuelve false
+
+TODO: Cómo hacerlo:
+-Crear una función con parámetro número
+-Convertir a String
+-Convertir número en array de números
+-Darle la vuelva
+-Unirlo y convertirlo a entero
+-Comparar si los valores son iguales
+*/
