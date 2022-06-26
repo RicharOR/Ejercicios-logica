a6c179bd08504195e526fe445fae789f50281d51
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-26 17:53:44 +0200

    Ejercicio 27 sin resolver

--- ej27CalcularDias.js
+++ ej27CalcularDias.js
@@ -10,13 +10,3 @@ TODO: Cómo hacerlo:
 -Crear una función con parámetro días
 -Divisiones para sacar años, meses y días
 */
-
-const calcularFechas = num => {
-  let years = parseInt(num / 365)
-  let months = parseInt((num % 365) / 30)
-  let days = parseInt((num % 365) % 30)
-
-  return `${num} equivale a ${years} años, ${months} meses y ${days} días`
-}
-
-console.log(calcularFechas(920))
