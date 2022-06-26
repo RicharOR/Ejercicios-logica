6e4bd602f9b9ef0b4b31788f710af664ab361879
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-26 17:43:50 +0200

    Ejercicio 8 sin resolver

--- ej8NumeroInvertido.js
+++ ej8NumeroInvertido.js
@@ -6,21 +6,8 @@ invertirNumero(67)  Devuelve: 76
 TODO: Cómo hacerlo:
 -Funcion que reciba el número
 -Convertir el número en string
--crear un array por cada letra
+-Crear un array por cada letra
 -Darle la vuelta
 -Unir el array en un string
 -Convertir el string en un entero
 */
-
-const invertirNumero = (numero) => {
-	let invertido = parseInt(
-                    numero
-                    .toString()
-                    .split('')
-                    .reverse()
-                    .join('')
-                    ) * Math.sign(numero);
-	return invertido;
-};
-
-console.log(invertirNumero(-987));
