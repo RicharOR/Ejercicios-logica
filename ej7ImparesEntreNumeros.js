b9ef4805aa845fb04fc901ae87b5df34d66423fb
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-26 17:43:11 +0200

    Ejercicio 7 sin resolver

--- ej7ImparesEntreNumeros.js
+++ ej7ImparesEntreNumeros.js
@@ -7,19 +7,6 @@ TODO: Como hacerlo:
 -Función que reciba los dos números
 -Bucle del "número1" al "número2"
 -Condición, si el resto es distinto a cero, es impar
--Aumentar en uno el contado
+-Aumentar en uno el contador
 -Devolver contador
 */
-
-function impares(num1, num2) {
-	let contador = 0;
-
-	while (num1 < num2) {
-		if (num1 % 2 != 0) contador++;
-
-		num1++;
-	}
-	return contador;
-}
-
-console.log(impares(7, 700));
