becbeeb3b5beef4fd3f7b769d25b541851f35b0a
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-26 18:13:02 +0200

    [x]Creado el ejercicio 30

---
+++ ej30EliminarDuplicados.js
@@ -0,0 +1,11 @@
+/* Dado un array de numero, devolver el array sin elementos duplicados
+Si hay un string eliminarlo del array
+
+Ejemplo:
+eliminarDuplicados([1,2,1,1,1,3,4]);  Devuelve: [1,2,3,4]
+
+TODO: Cómo hacerlo:
+-Crear una función con parametro array
+-Recorrer el array y eliminar los strings
+-Usar objeto para eliminar duplicados
+*/
