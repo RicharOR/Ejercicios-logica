f81232adb6be21c2b430d5b7e4f6d9dd13613812
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-26 16:03:39 +0200

    [x]Ejercicio 29 resuelto

--- ej29NumeroCapicua.js
+++ ej29NumeroCapicua.js
@@ -7,19 +7,7 @@ Ejemplos:
 capicua(2002)  Devuelve true
 capicua(2014)  Devuelve false
 
-TODO: Cómo hacerlo:
--Crear una función con parámetro número
--Convertir a String
--Convertir número en array de números
--Darle la vuelva
--Unirlo y convertirlo a entero
--Comparar si los valores son iguales
 */
-/* let capicua = 2002
-let toText = capicua.toString()
-let reversed = toText.split('').reverse().join('')
-let compare = reversed === toText */
-
 const capicua = num => {
   const toText = num.toString()
   const reversed = toText.split('').reverse().join('')
