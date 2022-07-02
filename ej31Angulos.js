097e8589f2c1c9648ab1eac3d38c27f2fef977f8
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-02 13:38:42 +0200

    [x]Cambio de nombre ejercicio 31

---
+++ ej31Angulos.js
@@ -0,0 +1,13 @@
+/* Dado un número, indicar que tipo de ángulo es
+
+Ejemplos:
+angulo(47)    Devuelve Angulo agudo
+angulo(90)    Devuelve Angulo recto
+angulo(145)   Devuelve Angulo obtuso
+angulo(180)   Devuelve Angulo llano
+
+TODO: Cómo hacerlo:
+-Crear una función con parámetro número
+-Condiciones para detectar el ángulo
+-Devolver el resultado
+*/
