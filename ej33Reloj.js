e62040f2cd354918b25c9c4ec811cbac457e146f
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-02 18:49:08 +0200

    [x]Creado el ejercicio 33

---
+++ ej33Reloj.js
@@ -0,0 +1,19 @@
+/* Haz un reloj que en tiempo real vaya mostrando la hora cada segundo
+
+Solo puedes usar el objeto date para conseguir la hora por primera vez, pero no para actualizar la hora
+
+Ejemplo:
+let miReloj = new Rejoj()
+mi_reloj.encender()
+
+14:20:14
+14:20:15
+14:20:16
+
+TODO: Cómo hacerlo:
+-Crear una clase
+-Inicializar propiedades con hora actual
+-Método con setInterval
+-Método para mostrar la hora
+-Método para sumar segundos a la hora y actualizar hora, minutos y segundos
+*/
