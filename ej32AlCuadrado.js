eb01c6df18c8a54e5bb740dda7da6bbd09384479
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-02 13:46:07 +0200

    [x]Ejercicio 32 sin resolver

---
+++ ej32AlCuadrado.js
@@ -0,0 +1,13 @@
+/* Dado un array de número, devolver el array con sus números elevados al cuadrado (número multiplicado por si mismo)
+
+El array debe eliminar cualquier contenido que no sea número
+
+Ejemplo:
+alCuadrado([5,6,7,"texto"]) Devuelve [25,36,49]
+
+TODO: Cómo hacerlo:
+-Crear una función con parámetro números
+-Filtrar array
+-Modificar contenido array
+-Devolver resultado
+*/
