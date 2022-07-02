887a440f7baef3f9b96629a11b5d668978b11cfa
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-02 18:33:32 +0200

    [x]Ejercicio 32 completado

--- ej32AlCuadrado.js
+++ ej32AlCuadrado.js
@@ -6,3 +6,14 @@ Ejemplo:
 alCuadrado([5,6,7,"texto"]) Devuelve [25,36,49]
 
 */
+
+const alCuadrado = arr => {
+  const filtered = arr
+    .filter(element => {
+      return typeof element !== 'string'
+    })
+    .map(element => element ** 2)
+  return filtered
+}
+
+console.log(alCuadrado([5, 'otro texto', 6, 7, 'texto', 8]))
