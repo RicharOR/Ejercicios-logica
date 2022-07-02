80f990de52a517b4aa5c6f0d867573f93bad5474
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-02 13:46:55 +0200

    [x]Texto modificado

--- ej32AlCuadrado.js
+++ ej32AlCuadrado.js
@@ -5,9 +5,4 @@ El array debe eliminar cualquier contenido que no sea número
 Ejemplo:
 alCuadrado([5,6,7,"texto"]) Devuelve [25,36,49]
 
-TODO: Cómo hacerlo:
--Crear una función con parámetro números
--Filtrar array
--Modificar contenido array
--Devolver resultado
 */
