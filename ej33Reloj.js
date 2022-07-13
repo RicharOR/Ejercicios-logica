6d7eb94726c0a9a8be49987652979c30ddddfb0a
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-13 17:31:31 +0200

    [x]Ejercicio 33 resuelto

--- ej33Reloj.js
+++ ej33Reloj.js
@@ -3,7 +3,7 @@
 Solo puedes usar el objeto date para conseguir la hora por primera vez, pero no para actualizar la hora
 
 Ejemplo:
-let miReloj = new Rejoj()
+let miReloj = new Reloj()
 mi_reloj.encender()
 
 14:20:14
@@ -17,3 +17,44 @@ TODO: Cómo hacerlo:
 -Método para mostrar la hora
 -Método para sumar segundos a la hora y actualizar hora, minutos y segundos
 */
+
+class Reloj {
+  constructor() {
+    this.fecha = new Date()
+    this.hora = this.fecha.getHours()
+    this.minutos = this.fecha.getMinutes()
+    this.segundos = this.fecha.getSeconds()
+  }
+
+  actualizar(segundos) {
+    this.segundos += segundos
+
+    if (this.segundos >= 60) {
+      this.minutos++
+      this.segundos = 0
+    }
+
+    if (this.minutos >= 60) {
+      this.hora++
+      this.minutos = 0
+    }
+
+    if (this.hora >= 24) {
+      this.hora = 0
+    }
+  }
+
+  mostrar() {
+    this.actualizar(1)
+    console.log(`${this.hora}:${this.minutos}:${this.segundos}`)
+  }
+
+  encender() {
+    setInterval(() => {
+      this.mostrar()
+    }, 1000)
+  }
+}
+
+let miReloj = new Reloj()
+miReloj.encender()
