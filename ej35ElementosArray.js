8a0964dd5564dd0ad6e56f7f61fc5077cb64389d
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-26 15:07:24 +0200

    [x]Ejercicio 35 resuelto

---
+++ ej35ElementosArray.js
@@ -0,0 +1,15 @@
+/* Dado un array crear uno nuevo con el primer y último elemento del original
+
+Ejemplo:
+primeroYUltimo([100,200,300,780]) Devuelve: [100,780]
+*/
+
+const primeroYUltimo = arr => {
+  let newArray = []
+  newArray.push(arr.at(0))
+  newArray.push(arr.at(-1))
+
+  return newArray
+}
+
+console.log(primeroYUltimo([50, 100, 200, 300, 780, 220]))
