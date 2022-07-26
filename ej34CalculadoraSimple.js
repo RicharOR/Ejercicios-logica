88cfbcdb0511b7b52160f931d12819e2779162cb
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-07-26 14:51:26 +0200

    [x]Creado el ejercicio 34

---
+++ ej34CalculadoraSimple.js
@@ -0,0 +1,11 @@
+/* Dados dos números devolver el resultado de las operaciones básicas entre ellos (suma, resta, multiplicación, división) 
+
+Ejemplo:
+calculadora(5,5)  
+
+Devuelve:
+La suma es: 10
+La resta es: 0
+La multiplicación es: 25
+La división es: 1
+*/
